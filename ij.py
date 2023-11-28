352d61972d32818ac4fcb704140465cd067a24bf
Bhim Dura <bhimprasaddura@gmail.com>
2023-11-28 16:43:11 +0545

    first commitment

---
+++ ij.py
@@ -0,0 +1,16 @@
+import requests
+
+class total:
+    def __init__(self):
+     self.url="https://foodmandu.com/webapi/api/v2/Product/GetVendorProductsBySubCategoryV2?VendorId={venid}&show="
+
+    def scrape(self,venid):
+       url = self.url.format(venid=venid)
+       r = requests.get(url)
+       data = r.json
+       data = data[0]['items']
+       for i in data:
+          print("name:",i['name'], i['price'])
+       return data
+s=total()
+s.scrape(844)
